196234a3367086173390cd99c2071654656e9d28
Javacasm <javacasm@gmail.com>
2020-06-13 21:51:30 +0200

    update

--- code/ControlEncendidoApagadoAutomaticoRiego.py
+++ code/ControlEncendidoApagadoAutomaticoRiego.py
@@ -1,9 +1,10 @@
 """
-Encendemos/apagamos el sistema de riego (bomba controlada por un relé) automáticamente
+Encendemos/apagamos el riego (bomba controlada por un relé) automáticamente
 según el valor de un sensor de humedad de suelo:
 * Por debajo de un valor de humedad se enciende el motor
 * Por encima se apaga
-* Se usa un sensor de lluvia para detectar si la planta ya está expulsando agua por abajo
+* Se usa un sensor de lluvia para detectar si la planta ya está expulsando
+    agua por abajo
 Controlamos el nivel de agua de un depósito con sensor de nivel de agua
 Si el nivel es bajo el motor se apaga automáticamente
 
@@ -43,9 +44,18 @@ imageEmpty = Image("90009:"
                    "90009:"
                    "99999")
 
+# Valores umbral para activaciones
 alarmaNivelAgua = 400
+humedadParaRiego = 500
+sensorLluviaMojado = 800
+
+humedadSuelo = 0
+valorSensorLluvia = 0
+nivelDeposito = 0
+
 
 def EnciendeRiego():
+    global nivelDeposito
     if nivelDeposito > alarmaNivelAgua:
         display.show(imageUp)
         pin16.write_digital(True)
@@ -54,20 +64,46 @@ def ApagaRiego():
     display.show(imageDown)
     pin16.write_digital(False)
 
+def EstaEncendidoRiego():
+    return pin16.value()
+
 def MostrarNivelAgua():
+    global nivelDeposito
     if nivelDeposito < alarmaNivelAgua:
+        if EstaEncendidoRiego() :
+            ApagaRiego()
         display.show(imageEmpty)
     elif nivelDeposito < 1.5 * alarmaNivelAgua:
         display.show(imageHalf)
     else:
         display.show(imageFull)
 
-
-while True:
+def MedirSensores():
     nivelDeposito = pin2.read_analog()
-    MostrarNivelAgua()
-    if button_a.is_pressed():
+    uart.write('NivelDeposito=' + str(nivelDeposito) + '\n')
+    valorSensorLluvia = pin1.read_analog()
+    uart.write('valorSensorLluvia=' + str(valorSensorLluvia) + '\n')
+    # El sensor de humedad es inversamente proporcional a la humedad
+    humedadSuelo = 1023 - pin0.read_analog()
+    uart.write('humedadSuelo=' + str(humedadSuelo) + '\n')
+
+
+def RevisarHumedadSuelo():
+    global humedadSuelo, valorSensorLluvia
+    if humedadSuelo < humedadParaRiego:
         EnciendeRiego()
-    if button_b.is_pressed():
+        # Mantenemos encendido el riego mientras se cumplan estas condiciones
+        while humedadSuelo < humedadParaRiego and valorSensorLluvia > sensorLluviaMojado:
+            MedirSensores()
         ApagaRiego()
 
+def miMain():
+    # uart.init()
+    while True:
+        MedirSensores()
+        MostrarNivelAgua()
+        RevisarHumedadSuelo()
+        if button_a.is_pressed():
+            EnciendeRiego()
+        if button_b.is_pressed():
+            ApagaRiego()
