f93f8a23ab37085758c123b184ffe0f8b85434e6
Javacasm <javacasm@gmail.com>
2020-06-13 19:52:57 +0200

    add

---
+++ code/ControlNivelAguaDeposito.py
@@ -0,0 +1,59 @@
+from microbit import *
+
+imageUp = Image("00900:"
+                "09990:"
+                "90909:"
+                "00900:"
+                "00900")
+
+imageDown = Image("00900:"
+                  "00900:"
+                  "90909:"
+                  "09990:"
+                  "00900")
+
+imageFull = Image("99999:"
+                  "99999:"
+                  "99999:"
+                  "99999:"
+                  "99999")
+
+imageHalf = Image("90009:"
+                  "90009:"
+                  "99999:"
+                  "99999:"
+                  "99999")
+
+imageEmpty = Image("90009:"
+                   "90009:"
+                   "90009:"
+                   "90009:"
+                   "99999")
+
+alarmaNivelAgua = 400
+
+def EnciendeRiego():
+    if nivelDeposito > alarmaNivelAgua:
+        display.show(imageUp)
+        pin16.write_digital(True)
+
+def ApagaRiego():
+    display.show(imageDown)
+    pin16.write_digital(False)
+
+def MostrarNivelAgua():
+    if nivelDeposito < alarmaNivelAgua:
+        display.show(imageEmpty)
+    elif nivelDeposito < 1.5 * alarmaNivelAgua:
+        display.show(imageHalf)
+    else:
+        display.show(imageFull)
+
+
+while True:
+    nivelDeposito = pin2.read_analog()
+    MostrarNivelAgua()
+    if button_a.is_pressed():
+        EnciendeRiego()
+    if button_b.is_pressed():
+        ApagaRiego()
